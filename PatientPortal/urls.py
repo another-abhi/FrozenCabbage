8c557b28b554326f7c67aff341fe020a49d94388
Abhishek Suresh <chuninpeter@yahoo.com>
2017-11-15 12:27:44 +0530

    login_register

--- PatientPortal/urls.py
+++ PatientPortal/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='patient_login'),
+    #url(r'^$', views.index, name='patient_login'),
+    url(r'^$', views.logina, name='patient_login'),
+
 ]
